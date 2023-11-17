ccd3d4dfe51b7a10e087457ce88a809414ec4869
KangHoML <kanghokhkh1@gmail.com>
2023-11-17 17:15:37 +0900

    complete self_play.py

--- self_play.py
+++ self_play.py
@@ -1,6 +1,7 @@
 import os
 import torch
 import pickle
+import argparse
 import numpy as np
 
 from datetime import datetime
@@ -17,9 +18,67 @@ def first_player_value(ended_state):
 
 # store the train data
 def write_data(history):
-    now = data
+    now = datetime.now()
+    os.makedirs('./data/', exist_ok=True)
+    path = f'./data/{now.year:04}{now.month:02}{now.day:02}{now.hour:02}{now.minute:02}{now.second:02}.history'
 
+    with open(path, mode='wb') as f:
+        pickle.dump(history, f)
 
-
-def play(net):
+# game play
+def play(model, temperature, pv_eval_count):
     history = []
+    state = State()
+
+    while True:
+        if state.is_done():
+            break
+
+        scores = pv_mcts_scores(model, state, temperature, pv_eval_count)
+
+        policies = [0] * 9 # output_size
+        for action, policy in zip(state.legal_actions(), scores):
+            policies[action] = policy
+        history.append([[state.pieces, state.enemy_pieces], policies, None])
+
+        action = np.random.choice(state.legal_actions(), p=scores)
+
+        state = state.next(action)
+
+    value = first_player_value(state)
+    for i in range(len(history)):
+        history[i][2] = value
+        value = -value
+        
+    return history
+
+# self play
+def self_play(args):
+    history = []
+
+    net = DualNetwork(num_residual_block=args.num_residual_block, num_filters=args.num_filters)
+    model = net
+    # model = net.load_state_dict(args.model_path)
+    net.eval()
+    
+    for i in range(args.self_count):
+        h = play(model, args.temperature, args.pv_eval_count)
+        history.extend(h)
+        print(f'\rSelfPlay {i+1}/{args.self_count}', end='')
+    
+    print('')
+
+    write_data(history)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Prepare and store the data to play self")
+
+    parser.add_argument('--model_path', type=str, default='./model/best.pth')
+    parser.add_argument('--pv_eval_count', type=int, default=50)
+    parser.add_argument('--temperature', type=float, default=1.0)
+    parser.add_argument('--num_residual_block', type=int, default=16)
+    parser.add_argument('--num_filters', type=int, default=128)
+    parser.add_argument('--self_count', type=int, default=500)
+
+    args = parser.parse_args()
+    self_play(args)
